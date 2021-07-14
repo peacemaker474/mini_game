f02f446e797908ac9f2e18cc1514ff9a1c2f4f98
peacemaker474 <81507322+peacemaker474@users.noreply.github.com>
2021-07-14 20:23:14 +0900

    Add files via upload

--- js/api.js
+++ js/api.js
@@ -1,20 +1,33 @@
-const KoreanApi = `69C1A75986C675AEFEC60B73B2DF3193`;
+const KoreanApi = `EE719FB21746F0F92434ACD6A929FC13`;
 
-function getKorea() {
+const getKorea = (lastWord) => {
     const xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET", `https://stdict.korean.go.kr/api/search.do?key=${KoreanApi}q=별밤&num=50&advanced=y&method=start&pos=1`, false);
+    xmlHttp.open("GET", `https://stdict.korean.go.kr/api/search.do?key=${KoreanApi}&q=${lastWord}&num=15&advanced=y&method=start&pos=1&letter_s=2&letter_e=6`, false);
     xmlHttp.send();
-    if(xmlHttp.status == 200) {
+    if (xmlHttp.status == 200) {
         const data = Array.from(xmlHttp.responseXML.all);
+        const check = data.filter(item => item.tagName === "total");
         const word = data.filter(item => item.tagName === "word");
-        let i = Math.round(Math.random() * 10);
-        console.log(word);
-        console.lod(data);
-        console.log(word[i].textContent);
-        console.log("통신 성공");
+        let i = Math.round(Math.random() * word.length);
+        if (check[0].textContent !== "0") {
+            if (!undefined) {
+                if (word.length === 1) {
+                    const computerWord = word[0].textContent;
+                    input.value = computerWord;
+                    computerInputWord(computerWord)
+                } else {
+                    const computerWordN = word[i].textContent;
+                    input.value = computerWordN;
+                    computerInputWord(computerWordN);
+                }
+                setTimeout(() => { input.value = input.value.slice(-1) }, 2000);
+            } else {
+                input.value = "승리자"
+            }
+        } else {
+            input.value = "승리자";
+        }
     } else {
         console.log("통신 실패");
     }
 }
-
-getKorea();
--- js/index.js
+++ js/index.js
@@ -1,12 +1,54 @@
 const form = document.querySelector("#input_word");
 const input = form.querySelector(".word");
 const human = document.querySelector(".human_word");
-const computer = document.querySelector("computer_word");
+const computer = document.querySelector(".computer_word");
 
 const KoreanApi = `EE719FB21746F0F92434ACD6A929FC13`;
 
-const humanInputWord = (text) => {
+const computerInputWord = (text) => {
+    const li = document.createElement("li");
+    const pattern = /[a-z0-9]|[ \[\]{}()<>?|`~!@#$%^&*-_+=,.;:\"'\\]/g;
+    text = text.replace(pattern, '');
+    li.textContent = text;
+    computer.appendChild(li);
+}
 
+const humanInputWord = (text) => {
+    const li = document.createElement("li");
+    li.textContent = text;
+    human.appendChild(li);
+}
+
+const getKorea = (lastWord) => {
+    const xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", `https://stdict.korean.go.kr/api/search.do?key=${KoreanApi}&q=${lastWord}&num=15&advanced=y&method=start&pos=1&letter_s=2&letter_e=6`, false);
+    xmlHttp.send();
+    if (xmlHttp.status == 200) {
+        const data = Array.from(xmlHttp.responseXML.all);
+        const check = data.filter(item => item.tagName === "total");
+        const word = data.filter(item => item.tagName === "word");
+        let i = Math.round(Math.random() * word.length);
+        if (check[0].textContent !== "0") {
+            if (!undefined) {
+                if (word.length === 1) {
+                    const computerWord = word[0].textContent;
+                    input.value = computerWord;
+                    computerInputWord(computerWord)
+                } else {
+                    const computerWordN = word[i].textContent;
+                    input.value = computerWordN;
+                    computerInputWord(computerWordN);
+                }
+                setTimeout(() => { input.value = input.value.slice(-1) }, 2000);
+            } else {
+                input.value = "승리자"
+            }
+        } else {
+            input.value = "승리자";
+        }
+    } else {
+        console.log("통신 실패");
+    }
 }
 
 const handleSubmitWord = (event) => {
@@ -15,33 +57,17 @@ const handleSubmitWord = (event) => {
     humanInputWord(currentValue);
     const lastWord = currentValue.slice(-1);
     input.value = lastWord;
-
-    function getKorea() {
-        const xmlHttp = new XMLHttpRequest();
-        xmlHttp.open("GET", `https://stdict.korean.go.kr/api/search.do?key=${KoreanApi}&q=${lastWord}&num=15&advanced=y&method=start&pos=1&letter_s=2`, false);
-        xmlHttp.send();
-        if (xmlHttp.status == 200) {
-            const data = Array.from(xmlHttp.responseXML.all);
-            const word = data.filter(item => item.tagName === "word");
-            let i = Math.round(Math.random() * word.length);
-            if (word[i].textContent.length === 1) {
-                input.value = word[0].textContent;
-                console.log(word[0].text, "성공");
-            } else {
-                input.value = word[i].textContent;
-                console.log(word, "실패");
-            }
-            setTimeout(() => { input.value = input.value.slice(-1) }, 2000);
-        } else {
-            console.log("통신 실패");
-        }
-    }
-
-    getKorea();
+    getKorea(lastWord);
 
 }
 
+const handleOnlyKorea = () => {
+    const pattern = /[a-z0-9]|[ \[\]{}()<>?|`~!@#$%^&*-_+=,.;:\"'\\]/g;
+    input.value = input.value.replace(pattern, '');
+}
+
 function startGame() {
+    input.addEventListener("keyup", handleOnlyKorea);
     form.addEventListener("submit", handleSubmitWord);
 }
 
