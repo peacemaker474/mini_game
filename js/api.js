55751fc7c3957871467979d571f76ae311f2fa79
peacemaker474 <81507322+peacemaker474@users.noreply.github.com>
2021-07-18 15:09:54 +0900

    2021.07.18
    전체적인 UI변경, Refactoring

--- js/api.js
+++ js/api.js
@@ -9,6 +9,7 @@ const computerInput = (lastWord) => {
             const xml = parser.parseFromString(data, "text/xml");
             const wordCheck = Array.from(xml.getElementsByTagName("total")); // 사전 단어 여부
             const getWord = Array.from(xml.getElementsByTagName("word")); // 단어 가지고 오기
+            const wordInfo = Array.from(xml.getElementsByTagName("definition")); // 설명 가지고 오기
             let i = 0; // 랜덤으로 번호 가지고 오기
             if (getWord.length === 1) {
                 i = Math.round(Math.random() * getWord.length) - 1; // 랜덤으로 가지고 오기
@@ -18,19 +19,35 @@ const computerInput = (lastWord) => {
 
             if (wordCheck[0].textContent !== "0") {
                 const computerWordN = getWord[i].textContent;
-                const word = computerWordN.replace(pattern, '');
-                const againWordCheck = wordList.indexOf(word);
-                if (againWordCheck === -1) {
-                    input.value = computerWordN;
-                    wordList.push(word);
-                    paintComputer(computerWordN);
-                    setTimeout(() => input.value = input.value.slice(-1), 2000);
+                const wordInfoN = wordInfo[i].textContent;
+                let wordDes; // 단어에 대한 설명이 길면, 줄이기 위한..
+                if (wordInfoN.length > 40) {
+                    const wordLength = `${wordInfoN.substring(0, 40)}...`;
+                    wordDes = wordLength;
                 } else {
+                    wordDes = wordInfoN;
+                }
+                const word = computerWordN.replace(pattern, '');
+                const againWordCheck = wordList.indexOf(word); // 중복 단어 체크
+                if (againWordCheck === -1) { // 중복 단어 없을 시 게임은 계속,
+                    wordList.push(word);
+                    paintWord(computerWordN);
+                    description.textContent = wordDes;
+                    beforeAfter = word;
+                    computerCheck.textContent = "";
+                    setTimeout(() => input.value = computerWordN.slice(-1), 2000);
+                } else { // 있을 시 다시 입력할 수 있도록.
                     computerInput(lastWord);
                     input.value = lastWord;
                 }
             } else {
                 location.reload(alert("사람 승리"));
             }
+        })
+        .catch((e) => { // 오류가 발생해도 마찬가지로 다시 입력하도록.
+            computerInput(lastWord);
+            input.value = lastWord;
+            computerCheck.textContent = "중복된 단어로 다시 입력하겠습니다."
+            wordList.pop();
         })
 }
--- js/index.js
+++ js/index.js
@@ -1,11 +1,14 @@
 const form = document.querySelector("#input_word");
-const input = form.querySelector(".word");
-const human = document.querySelector(".human_word");
-const computer = document.querySelector(".computer_word");
+const input = form.querySelector(".word"); // 단어 입력창
+const currentWord = document.querySelector(".current_word"); // 단어
+const description = document.querySelector(".word_description"); // 단어 설명
+const humanCheck = document.querySelector(".human_check"); // 중복 단어 체크 li
+const computerCheck = document.querySelector(".computer_check"); // 중복 단어 체크 li
 
 const pattern = /[a-z0-9]|[ \[\]{}()<>?|`~!@#$%^&*-_+=,.;:\"'\\]/g; // 한글만 입력, 정규표현식
-let whosWin = false; // 내가 입력한 단어 사전 유무체크
-let wordList = [];
+let myWordB = false; // 내가 입력한 단어 사전 유무체크
+let wordList = []; // 중복 단어 체크
+let beforeAfter = ""; // 단어 앞뒤 체크
 
 // 사전 유무 체크
 const checkWord = (inputWord) => {
@@ -18,54 +21,56 @@ const checkWord = (inputWord) => {
         const sameWord = data.filter(item => item.tagName === "word");
         const expectWord = sameWord.map(item => item.textContent = item.textContent.replace(pattern, ''));
         if (checkInput[0].textContent !== "0") {
-            const inputCheck = expectWord.map(item => item === inputWord);
-            const LastCheck = inputCheck.filter(item => item === true);
+            const LastCheck = expectWord
+                .map(item => item === inputWord)
+                .filter(item => item === true);
             if (LastCheck[0]) {
-                wordList.push(inputWord);
-                return whosWin = true;
+                wordList.push(inputWord); // 중복 단어 체크를 위한 배열
+                paintWord(inputWord); // 내가 입력한 단어 보여주기
+                humanCheck.textContent = ""; // 만약 중복 단어가 있고, 다시 입력했을시 빈값으로 만들어줌.
+                return myWordB = true;
             }
         } else {
-            return whosWin = false;
+            return myWordB = false;
         }
+    } else {
+        console.log("데이터를 받아오지 못 했수다.");
     }
 }
 
-// 컴퓨터 입력시 ul 반영
-const paintComputer = (text) => {
-    const li = document.createElement("li");
+// 현재 단어 및 설명 보여주기
+const paintWord = (text) => {
     text = text.replace(pattern, '');
-    li.textContent = text;
-    computer.appendChild(li);
-}
-// 사람 입력시 ul 반영
-const paintHuman = (text) => {
-    const li = document.createElement("li");
-    li.textContent = text;
-    human.appendChild(li);
+    currentWord.textContent = text;
 }
 
 const handleSubmitWord = (event) => {
     event.preventDefault();
     let currentValue = input.value;
     const sendValue = currentValue;
-    const lastWord = currentValue.slice(-1);
+    const lastWord = currentValue.slice(-1); // 단어 뒷 글자
+    const forWard = currentValue.slice(0, 1); // 단어 앞 글자
     let checking;
     if (wordList.length >= 2) {
         const againWordCheck = wordList.find(item => item === currentValue);
         checking = againWordCheck;
     }
-    if (!checking) { // 중복 단어 체크
-        checkWord(currentValue);
-        if (whosWin) {
-            paintHuman(currentValue);
-            input.value = lastWord;
-            computerInput(lastWord, sendValue);
+    if (beforeAfter.length === 0 || forWard === beforeAfter.slice(-1)) { // 앞뒤 단어 체크
+        if (!checking) { // 중복 단어 체크
+            checkWord(currentValue);
+            if (myWordB) { // 내가 입력한 단어가 사전에 있을시, 게임은 계속
+                computerInput(lastWord, sendValue);
+                description.textContent = "";
+            } else { // 없을 시 게임은 끝
+                location.reload(alert("컴퓨터 승리"));
+            }
         } else {
-            location.reload(alert("컴퓨터 승리"));
+            currentValue = "";
+            input.value = "";
+            humanCheck.textContent = "중복된 단어가 있습니다.";
         }
     } else {
-        currentValue = "";
-        input.value = "";
+        alert("다시 입력하세요");
     }
 }
 
